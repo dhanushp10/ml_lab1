be70f6d9a84c4d4349b20e36dcf3001ba9e8cf7e
dhanushp10 <161063940+dhanushp10@users.noreply.github.com>
2025-07-09 12:37:53 +0530

    Add files via upload

--- q5.py
+++ q5.py
@@ -1,5 +1,11 @@
 #to generate a random number using random
 
+def most_frequent(List):
+    return max(set(List), key=List.count)
+
+List = [2, 1, 2, 2, 1, 3]
+print(most_frequent(List))
+
 import random
 l=[]
 for i in range(0,100):
@@ -10,3 +16,5 @@ print("the mean is ",sum(l)/len(l))
 print(l.sort())
 n=len(l)
 print("the median is",(l[50]+l[51])/2)
+
+print("the mode is ",most_frequent(l))
