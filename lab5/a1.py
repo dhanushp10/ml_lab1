65c852d2a1f1bd4804349af6bd6d0c4ed2fd07e8
Dhanush P <dhanusharibabu@gmail.com>
2025-08-06 21:42:12 +0530

    upto a7 completed

--- lab5/a1.py
+++ lab5/a1.py
@@ -1,6 +1,3 @@
-
-
-
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
 
@@ -39,10 +36,14 @@ col = [
     "std_idle",
     "class1"
 ]
-df=pd.read_csv("ml_lab1/lab3/lab3_vpn_nonvpndataset.txt",names=col)
+# df=pd.read_csv("ml_lab1/lab3/lab3_vpn_nonvpndataset.txt",names=col)
+df=pd.read_csv(r"C:\Users\dhanu\OneDrive\Desktop\jup\ml_lab1\2016_dataset\combine.txt",names=col)
 
-#shuffling of data using frac
-# res = df.sample(frac=1).reset_index(drop=True)
+print("shape of df before removing nan values",df.shape)
+df=df.dropna()
+print("shape of df after removing nan values",df.shape)
+
+#
 
 X,y=df.loc[:,["min_idle"]],df["std_idle"]
 
@@ -51,9 +52,9 @@ xtrain,xtest,ytrain,ytest=train_test_split (X, y, test_size=0.33, random_state=4
 
 
 reg =  LinearRegression().fit(xtrain, ytrain)
-y_train_pred = reg.predict(xtest)
+y_test_pred = reg.predict(xtest)
 print(len(ytest))
-print(len(y_train_pred))
+print(len(y_test_pred))
 
 
 #a2
@@ -64,7 +65,7 @@ def evaluate_price_prediction(actual, predicted): # Function to calculate metric
     r2 = r2_score(actual, predicted)
     return mse, rmse, mape, r2
 
-mse,rmse,mape,r2=evaluate_price_prediction(ytest,y_train_pred)
+mse,rmse,mape,r2=evaluate_price_prediction(ytest,y_test_pred)
 
 print(mse,rmse,r2,mape)
 
@@ -74,9 +75,9 @@ xtrain,xtest,ytrain,ytest=train_test_split (A, b, test_size=0.33, random_state=4
 
 
 reg =  LinearRegression().fit(xtrain, ytrain)
-y_train_pred = reg.predict(xtest)
+y_test_pred = reg.predict(xtest)
 print(len(ytest))
-print(len(y_train_pred))
+print(len(y_test_pred))
 
 
 def evaluate_price_prediction(actual, predicted): # Function to calculate metrics
@@ -86,7 +87,7 @@ def evaluate_price_prediction(actual, predicted): # Function to calculate metric
     r2 = r2_score(actual, predicted)
     return mse, rmse, mape, r2
 
-mse,rmse,mape,r2=evaluate_price_prediction(ytest,y_train_pred)
+mse,rmse,mape,r2=evaluate_price_prediction(ytest,y_test_pred)
 
 print(mse,rmse,r2,mape)
 
@@ -101,3 +102,37 @@ print(kmeans.labels_)
 print(kmeans.cluster_centers_)
 
 
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.metrics import davies_bouldin_score
+
+kmeans = KMeans(n_clusters=2, random_state=42).fit(attr)
+a=silhouette_score(attr, kmeans.labels_)
+c=calinski_harabasz_score(attr, kmeans.labels_)
+d=davies_bouldin_score(attr, kmeans.labels_)
+print(a,c,d)
+
+#a6
+sil_score=list()
+cal_score=list()
+davi_score=list()
+for i in range(2,7):
+  kmeans=KMeans(n_clusters=i,random_state=42).fit(attr)
+  a=silhouette_score(attr, kmeans.labels_)
+  sil_score.append(a)
+  c=calinski_harabasz_score(attr, kmeans.labels_)
+  cal_score.append(c)
+  d=davies_bouldin_score(attr, kmeans.labels_)
+  davi_score.append(d)
+
+x=np.array(range(2,7))
+plt.plot(x,sil_score)
+plt.plot(x,cal_score)
+plt.plot(x,davi_score)
+
+#a7
+distorsions=list()
+for k in range(2, 20):
+   kmeans = KMeans(n_clusters=k).fit(attr)
+   distorsions.append(kmeans.inertia_)
+plt.plot(distorsions)
